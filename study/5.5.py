c9f561d72a31b97cdd33566c746d2396b9baa953
daasik <dashakuzmina2000@mail.ru>
2021-04-15 21:04:41 +0300

    bb

--- study/5.5.py
+++ study/5.5.py
@@ -2,10 +2,39 @@ import pandas as pd
 
 pd.read_csv('Salaries.csv')
 sal = pd.DataFrame(pd.read_csv('Salaries.csv'))
-print(sal.info())
+#print(sal.info())
+#print(sal.head(3).to_string())
 # print(sal["BasePay"].mean())
 # print(sal['OvertimePay'].max())
 
 #print(sal[sal['EmployeeName'] == 'JOSEPH DRISCOLL']['TotalPayBenefits'])
-a = sal[sal['TotalPayBenefits'] == sal['TotalPayBenefits'].min()]
-print(a)
+#a = sal[sal['TotalPayBenefits'] == sal['TotalPayBenefits'].min()]
+#print(a)
+
+
+gb = sal.groupby('Year')['BasePay'].mean()
+#print(gb)
+
+a = len(sal['JobTitle'].unique())
+#print(a)
+
+c = sal['JobTitle'].value_counts().head(5)
+#print(c)
+
+o = sum(sal[sal['Year']==2013]['JobTitle'].value_counts()==1)
+#print(o)
+
+
+
+def chief_string(title):
+    if 'chief' in title.lower().split():
+        return True
+    else :
+        return False
+
+
+ch = sum(sal['JobTitle'].apply(lambda x: chief_string(x)))
+#print(ch)
+
+sal['title_len'] = sal['JobTitle'].apply(len)
+print(sal[['TotalPayBenefits','title_len']].corr())
---
+++ study/6.6.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+print(pd.read_csv('Ecommerce Purchases').head(10).to_string())
